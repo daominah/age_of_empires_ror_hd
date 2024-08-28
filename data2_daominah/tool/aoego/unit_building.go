894a237ab45227e845db2ba9c4c66083f7b857df
DaoThanhTung <tung.daothanhtung@gmail.com>
2024-08-28 23:51:34 +0700

    more Technology

--- data2_daominah/tool/aoego/unit_building.go
+++ data2_daominah/tool/aoego/unit_building.go
@@ -66,62 +66,64 @@ func (id UnitID) IntID() int { return int(id) }
 
 // UnitID enum
 const (
-	TownCenterID UnitID = 109
-	HouseID      UnitID = 70
-	GranaryID    UnitID = 68
-	StoragePitID UnitID = 103
-	BarracksID   UnitID = 12
-	DockID       UnitID = 45
+	TownCenter UnitID = 109
+	House      UnitID = 70
+	Granary    UnitID = 68
+	StoragePit UnitID = 103
+	Barracks   UnitID = 12
+	Dock       UnitID = 45
 
-	ArcheryRangeID UnitID = 87
-	StableID       UnitID = 101
-	MarketID       UnitID = 84
-	FarmID         UnitID = 50
-	TowerID        UnitID = 79
-	WallID         UnitID = 72
+	ArcheryRange UnitID = 87
+	Stable       UnitID = 101
+	Market       UnitID = 84
+	Farm         UnitID = 50
+	Tower        UnitID = 79
+	Wall         UnitID = 72
 
-	GovernmentCenterID UnitID = 82
-	TempleID           UnitID = 104
-	SiegeWorkshopID    UnitID = 49
-	AcademyID          UnitID = 0
+	GovernmentCenter UnitID = 82
+	Temple           UnitID = 104
+	SiegeWorkshop    UnitID = 49
+	Academy          UnitID = 0
 
-	WonderID UnitID = 276
+	Wonder UnitID = 276
 
-	VillagerID UnitID = 83
+	Villager UnitID = 83
 
-	SwordsmanID UnitID = 75
+	Clubman   UnitID = 73
+	Swordsman UnitID = 75
+	Slinger   UnitID = 347
 
-	BowmanID         UnitID = 4
-	ImprovedBowmanID UnitID = 5
-	ChariotArcherID  UnitID = 41
-	HorseArcherID    UnitID = 39
-	ElephantArcherID UnitID = 25
+	Bowman         UnitID = 4
+	ImprovedBowman UnitID = 5
+	ChariotArcher  UnitID = 41
+	HorseArcher    UnitID = 39
+	ElephantArcher UnitID = 25
 
-	ScoutID    UnitID = 299
-	ChariotID  UnitID = 40
-	CavalryID  UnitID = 37
-	ElephantID UnitID = 46
-	CamelID    UnitID = 338
+	Scout    UnitID = 299
+	Chariot  UnitID = 40
+	Cavalry  UnitID = 37
+	Elephant UnitID = 46
+	Camel    UnitID = 338
 
-	PriestID UnitID = 125
+	Priest UnitID = 125
 
-	StoneThrowerID UnitID = 35
-	BallistaID     UnitID = 11
+	StoneThrower UnitID = 35
+	Ballista     UnitID = 11
 
-	HopliteID UnitID = 93
+	Hoplite UnitID = 93
 
-	NullUnitID UnitID = -1
+	NullUnit UnitID = -1
 )
 
 func CheckIsBuilding(id UnitID) bool {
 	switch id {
-	case TownCenterID, HouseID, GranaryID, StoragePitID, BarracksID, DockID:
+	case TownCenter, House, Granary, StoragePit, Barracks, Dock:
 		return true
-	case ArcheryRangeID, StableID, MarketID, FarmID, TowerID, WallID:
+	case ArcheryRange, Stable, Market, Farm, Tower, Wall:
 		return true
-	case GovernmentCenterID, TempleID, SiegeWorkshopID, AcademyID:
+	case GovernmentCenter, Temple, SiegeWorkshop, Academy:
 		return true
-	case WonderID:
+	case Wonder:
 		return true
 	default:
 		return false
@@ -134,147 +136,155 @@ func CheckIsBuilding(id UnitID) bool {
 func NewUnit(id UnitID) *Unit {
 	u := &Unit{
 		ID:           id,
-		Location:     NullUnitID, // will be corrected later in the switch
+		Location:     NullUnit, // will be corrected later in the switch
 		IsBuilding:   CheckIsBuilding(id),
-		InitiateTech: NullTechID,
+		InitiateTech: NullTech,
 	}
 	switch id {
-	case VillagerID:
+	case Villager:
 		u.NameInGame, u.Name = "Villager", "Man"
 		u.Cost = Cost{Food: 50}
-		u.Time, u.Location = 20, TownCenterID
+		u.Time, u.Location = 20, TownCenter
 
-	case SwordsmanID:
+	case Clubman:
+		u.NameInGame, u.Name = "Clubman", "Soldier-Inf1"
+		u.Cost = Cost{Food: 50}
+		u.Time, u.Location = 26, Barracks
+	case Swordsman:
 		u.NameInGame, u.Name = "Short Swordsman", "Soldier-Inf3"
 		u.Cost = Cost{Food: 35, Gold: 15}
-		u.Time, u.Location = 26, BarracksID
+		u.Time, u.Location = 26, Barracks
+	case Slinger:
+		u.NameInGame, u.Name = "Slinger", "Soldier-Slinger"
+		u.Cost = Cost{Food: 40, Stone: 10}
+		u.Time, u.Location = 24, Barracks
 
-	case BowmanID:
+	case Bowman:
 		u.NameInGame, u.Name = "Bowman", "Soldier-Archer1"
 		u.Cost = Cost{Wood: 20, Food: 40}
-		u.Time, u.Location = 30, ArcheryRangeID
-	case ImprovedBowmanID:
+		u.Time, u.Location = 30, ArcheryRange
+	case ImprovedBowman:
 		u.NameInGame, u.Name = "Improved Bowman", "Soldier-Archer2"
 		u.Cost = Cost{Food: 40, Gold: 20}
-		u.Time, u.Location = 30, ArcheryRangeID
-	case ChariotArcherID:
+		u.Time, u.Location = 30, ArcheryRange
+	case ChariotArcher:
 		u.NameInGame, u.Name = "Chariot Archer", "Soldier-Chariot2"
 		u.Cost = Cost{Wood: 70, Food: 40}
-		u.Time, u.Location = 40, ArcheryRangeID
-	case HorseArcherID:
+		u.Time, u.Location = 40, ArcheryRange
+	case HorseArcher:
 		u.NameInGame, u.Name = "Horse Archer", "Soldier-Cavalry3_Arc"
 		u.Cost = Cost{Food: 50, Gold: 70}
-		u.Time, u.Location = 40, ArcheryRangeID
-	case ElephantArcherID:
+		u.Time, u.Location = 40, ArcheryRange
+	case ElephantArcher:
 		u.NameInGame, u.Name = "Elephant Archer", "Soldier-Elephant1"
 		u.Cost = Cost{Food: 180, Gold: 60}
-		u.Time, u.Location = 50, ArcheryRangeID
+		u.Time, u.Location = 50, ArcheryRange
 
-	case ScoutID:
+	case Scout:
 		u.NameInGame, u.Name = "Scout", "Soldier-Scout"
 		u.Cost = Cost{Food: 100}
-		u.Time, u.Location = 30, StableID
-	case ChariotID:
+		u.Time, u.Location = 30, Stable
+	case Chariot:
 		u.NameInGame, u.Name = "Chariot", "Soldier-Chariot1"
 		u.Cost = Cost{Wood: 60, Food: 40}
-		u.Time, u.Location = 40, StableID
-	case CavalryID:
+		u.Time, u.Location = 40, Stable
+	case Cavalry:
 		u.NameInGame, u.Name = "Cavalry", "Soldier-Cavalry1"
 		u.Cost = Cost{Food: 70, Gold: 80}
-		u.Time, u.Location = 40, StableID
-	case ElephantID:
+		u.Time, u.Location = 40, Stable
+	case Elephant:
 		u.NameInGame, u.Name = "War Elephant", "Soldier-Elephant"
 		u.Cost = Cost{Food: 170, Gold: 40}
-		u.Time, u.Location = 50, StableID
-	case CamelID:
+		u.Time, u.Location = 50, Stable
+	case Camel:
 		u.NameInGame, u.Name = "Camel Rider", "Soldier-Camel"
 		u.Cost = Cost{Food: 70, Gold: 60}
-		u.Time, u.Location = 30, StableID
+		u.Time, u.Location = 30, Stable
 
-	case PriestID:
+	case Priest:
 		u.NameInGame, u.Name = "Priest", "Priest"
 		u.Cost = Cost{Gold: 125}
-		u.Time, u.Location = 50, TempleID
+		u.Time, u.Location = 50, Temple
 
-	case StoneThrowerID:
+	case StoneThrower:
 		u.NameInGame, u.Name = "Stone Thrower", "Soldier-Catapult1"
 		u.Cost = Cost{Wood: 180, Gold: 80}
-		u.Time, u.Location = 60, SiegeWorkshopID
-	case BallistaID:
+		u.Time, u.Location = 60, SiegeWorkshop
+	case Ballista:
 		u.NameInGame, u.Name = "Ballista", "Soldier-Ballista"
 		u.Cost = Cost{Wood: 100, Gold: 80}
-		u.Time, u.Location = 60, SiegeWorkshopID
+		u.Time, u.Location = 60, SiegeWorkshop
 
-	case HopliteID:
+	case Hoplite:
 		u.NameInGame, u.Name = "Hoplite", "Soldier-Phal1"
 		u.Cost = Cost{Food: 60, Gold: 40}
-		u.Time, u.Location = 36, AcademyID
+		u.Time, u.Location = 36, Academy
 
-	case TownCenterID:
+	case TownCenter:
 		u.NameInGame, u.Name = "Town Center", "Town_Center1"
 		u.Cost = Cost{Wood: 200}
 		u.Time = 60
-	case HouseID:
+	case House:
 		u.NameInGame, u.Name = "House", "House"
 		u.Cost = Cost{Wood: 30}
 		u.Time = 20
-	case GranaryID:
+	case Granary:
 		u.NameInGame, u.Name = "Granary", "Granary"
 		u.Cost = Cost{Wood: 120}
 		u.Time = 30
-	case StoragePitID:
+	case StoragePit:
 		u.NameInGame, u.Name = "Storage Pit", "Storage_Pit1"
 		u.Cost = Cost{Wood: 120}
 		u.Time = 30
-	case BarracksID:
+	case Barracks:
 		u.NameInGame, u.Name = "Barracks", "Barracks1"
 		u.Cost = Cost{Wood: 125}
 		u.Time = 30
-	case DockID:
+	case Dock:
 		u.NameInGame, u.Name = "Dock", "Dock_1"
 		u.Cost = Cost{Wood: 100}
 		u.Time = 50
-	case ArcheryRangeID:
+	case ArcheryRange:
 		u.NameInGame, u.Name = "Archery Range", "Range1"
 		u.Cost = Cost{Wood: 150}
 		u.Time = 40
-	case StableID:
+	case Stable:
 		u.NameInGame, u.Name = "Stable", "Stable1"
 		u.Cost = Cost{Wood: 150}
 		u.Time = 40
-	case MarketID:
+	case Market:
 		u.NameInGame, u.Name = "Market", "Market1"
 		u.Cost = Cost{Wood: 150}
 		u.Time = 40
-	case FarmID:
+	case Farm:
 		u.NameInGame, u.Name = "Farm", "Farm"
 		u.Cost = Cost{Wood: 75}
 		u.Time = 30
-	case TowerID:
+	case Tower:
 		u.NameInGame, u.Name = "Watch Tower", "Watch_Tower"
 		u.Cost = Cost{Stone: 150}
 		u.Time = 80
-	case WallID:
+	case Wall:
 		u.NameInGame, u.Name = "Small Wall", "Wall_Small"
 		u.Cost = Cost{Stone: 5}
 		u.Time = 7
-	case GovernmentCenterID:
+	case GovernmentCenter:
 		u.NameInGame, u.Name = "Government Center", "Government_Center"
 		u.Cost = Cost{Wood: 175}
 		u.Time = 60
-	case TempleID:
+	case Temple:
 		u.NameInGame, u.Name = "Temple", "Temple1"
 		u.Cost = Cost{Wood: 200}
 		u.Time = 60
-	case SiegeWorkshopID:
+	case SiegeWorkshop:
 		u.NameInGame, u.Name = "Siege Workshop", "Siege_Workshop"
 		u.Cost = Cost{Wood: 200}
 		u.Time = 60
-	case AcademyID:
+	case Academy:
 		u.NameInGame, u.Name = "Academy", "Academy"
 		u.Cost = Cost{Wood: 200}
 		u.Time = 60
-	case WonderID:
+	case Wonder:
 		u.NameInGame, u.Name = "Wonder", "Wonder"
 		u.Cost = Cost{Wood: 1000, Gold: 1000, Stone: 1000}
 		u.Time = 8000
