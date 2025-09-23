7cad6e344cf8314dfc2ad91428ac6cc9bed6598b
DaoThanhTung <tung.daothanhtung@gmail.com>
2025-09-24 01:27:22 +0700

    WIP add Overpowered Mace; fix Shang attack reload should not apply to Scout and Camel.

--- switch_data_definitive_edition.sh
+++ switch_data_definitive_edition.sh
@@ -1,37 +1,53 @@
 #!/bin/bash
 
 # Script to switch the data file (units, buildings, techs, civs stats) between:
-# - Age of Empires: Rise of Rome v1.0 (default)
-# - Age of Empires: Definitive Edition
+# - Age of Empires: Rise of Rome v1.0
+# - Age of Empires: Definitive Edition and Return of Rome balance
+# - Overpowered Mace: civ Macedonian has combined bonuses from all civilizations
 
-# set to "true" to switch to Definitive Edition
-# is_change_to_definitive_edition=false
-is_change_to_definitive_edition=true
 
-definitive_edition="data/empires_definitive_edition.dat"  # switch_dat_to_aoe_de
-rise_of_rome="data/empires.dat.backup"                    # switch_dat_to_aoe_ror_v1.0
+TARGET="data/empires.dat"  # actual data file used by the game
 
-target="data/empires.dat"  # actual data file used by the game
+BRANCH_RISE_OF_ROME="data/empires.dat.backup"               # Rise of Rome v1.0
+BRANCH_DEFINITIVE="data/empires_definitive_edition.dat"     # Definitive Edition & Return of Rome
+BRANCH_MACE_OVERPOWERED="data/empires_mace_overpowered.dat" # Overpowered Mace
 
-if [ "$is_change_to_definitive_edition" = true ]; then
-    if [ -f "$definitive_edition" ]; then
-        cp -f "$definitive_edition" "$target"
+
+# SET HERE BEFORE RUNNING THE SCRIPT one of the three options:
+data_branch="$BRANCH_MACE_OVERPOWERED"
+
+if [ "$data_branch" = "$BRANCH_RISE_OF_ROME" ]; then
+    if [ -f "$BRANCH_RISE_OF_ROME" ]; then
+        cp -f "$BRANCH_RISE_OF_ROME" "$TARGET"
+        echo "$(date --iso=s) switched to Rise of Rome v1.0 data file."
+    else
+        echo "ERROR: file not found: $BRANCH_RISE_OF_ROME"
+        exit 1
+    fi
+elif [ "$data_branch" = "$BRANCH_DEFINITIVE" ]; then
+    if [ -f "$BRANCH_DEFINITIVE" ]; then
+        cp -f "$BRANCH_DEFINITIVE" "$TARGET"
         echo "$(date --iso=s) switched to Definitive Edition data file."
     else
-        echo "Definitive Edition data file not found: $definitive_edition"
+        echo "ERROR: file not found: $BRANCH_DEFINITIVE"
+        exit 1
+    fi
+elif [ "$data_branch" = "$BRANCH_MACE_OVERPOWERED" ]; then
+    if [ -f "$BRANCH_MACE_OVERPOWERED" ]; then
+        cp -f "$BRANCH_MACE_OVERPOWERED" "$TARGET"
+        echo "$(date --iso=s) switched to Overpowered Macedonian data file."
+    else
+        echo "ERROR: file not found: $BRANCH_MACE_OVERPOWERED"
         exit 1
     fi
 else
-    if [ -f "$rise_of_rome" ]; then
-        cp -f "$rise_of_rome" "$target"
-        echo "$(date --iso=s) switched to Rise of Rome v1.0 data file."
-    else
-        echo "Rise of Rome v1.0 data file not found: $rise_of_rome"
-        exit 1
-    fi
+    echo "unknown data branch: $data_branch, make sure it is a file path"
+    exit 1
 fi
 
-# use the following command ignore change to empires.dat while you are editing:
-# git update-index --skip-worktree data/empires.dat
-# to track changes again:
-# git update-index --no-skip-worktree data/empires.dat
+# - use the following command ignore change to empires.dat while you are editing:
+#   git update-index --skip-worktree data/empires.dat
+# - to track changes again:
+#   git update-index --no-skip-worktree data/empires.dat
+# - to list all files marked as skip:
+#   git ls-files -v . | grep ^S
